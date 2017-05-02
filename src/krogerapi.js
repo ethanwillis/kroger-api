780d7137685e41d9f17bf342326ab67450c4ca39
Matthew Longley <randomticktock@gmail.com>
2017-05-02 11:35:37 -0500

    switched everything to using a dedicated configuration file

--- src/krogerapi.js
+++ src/krogerapi.js
@@ -3,9 +3,14 @@ var fs = require("fs");
 
 class KrogerAPI {
 	constructor() {
-		// read in categories from JSON file
-		var categoriesObj = JSON.parse(fs.readFileSync('krogercategories.json', 'utf8'));
-		// store them in KrogerAPI object
-		this.categories = categoriesObj['categories'];
+		// read in configuration from JSON file
+		var configurationObj = JSON.parse(fs.readFileSync('krogerconf.json', 'utf8'));
+		// store categories in KrogerAPI object
+		this.categories = configurationObj['categories'];
+		// store paths in KrogerAPI object
+		this.base_url = configurationObj['paths']['base_url'];
+		this.auth_base_uri = configurationObj['paths']['auth_base_uri'];
+		this.api_base_uri = configurationObj['paths']['api_base_uri'];
+		this.products_uri = configurationObj['paths']['products_uri'];
 	}
 }
