8d873c477d6b13c13fcf42596870f777850d54e7
Matthew Longley <randomticktock@gmail.com>
2017-05-04 14:53:11 -0500

    added timeout so storefinder.js can fetch all stores sequentially

--- src/storefinder.js
+++ src/storefinder.js
@@ -8,42 +8,63 @@ var ziplist = (zipcodes.radius(38134, 10000000)).filter(function(code) {
 });
 
 // template for requests to find stores in a certain zip code
-;
+//var storeSearchReq = {
+	//baseUrl: 'https://www.kroger.com',
+	//uri: '/stores/api/graphql',
+	//jar: true,
+	//json: true,
+	//method: "POST",
+	//headers: {
+		//'Content-Type': 'application/json'
+	//},
+	//body: {
+		//'query': "query storeSearch($searchText: String!, $filters: [String]!) {  storeSearch(searchText: $searchText, filters: $filters) {    stores {      ...storeSearchResult    }    fuel {      ...storeSearchResult    }    shouldShowFuelMessage  }}fragment storeSearchResult on Store {  banner  vanityName  divisionNumber  storeNumber  phoneNumber  showWeeklyAd  showShopThisStoreAndPreferredStoreButtons  distance  latitude  longitude  address {    addressLine1    addressLine2    city    countryCode    stateCode    zip  }  pharmacy {    phoneNumber  }}",
+		//'variables': { 'searchText': "38134", 'filters': [] },
+		//'operationName': "storeSearch"
+	//}
+//};
+
+var currTimeout = 0;
+
+function ZipcodeRequest(zip) {
+	this.baseUrl = 'https://www.kroger.com';
+	this.uri = '/stores/api/graphql';
+	this.jar = true;
+	this.json = true;
+	this.method = 'POST';
+	this.headers = {
+		'Content-Type': 'application/json'
+	};
+	this.body = {
+		'query': "query storeSearch($searchText: String!, $filters: [String]!) {  storeSearch(searchText: $searchText, filters: $filters) {    stores {      ...storeSearchResult    }    fuel {      ...storeSearchResult    }    shouldShowFuelMessage  }}fragment storeSearchResult on Store {  banner  vanityName  divisionNumber  storeNumber  phoneNumber  showWeeklyAd  showShopThisStoreAndPreferredStoreButtons  distance  latitude  longitude  address {    addressLine1    addressLine2    city    countryCode    stateCode    zip  }  pharmacy {    phoneNumber  }}",
+		'variables': {
+			'searchText': zip,
+			'filters': []
+		},
+		'operationName': 'storeSearch'
+	};
+}
 
 var getStoresByZip = function(zip) {
-	var storeSearchReq = {
-		baseUrl: 'https://www.kroger.com',
-		uri: '/stores/api/graphql',
-		jar: true,
-		json: true,
-		method: "POST",
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		body: {
-			'query': "query storeSearch($searchText: String!, $filters: [String]!) {  storeSearch(searchText: $searchText, filters: $filters) {    stores {      ...storeSearchResult    }    fuel {      ...storeSearchResult    }    shouldShowFuelMessage  }}fragment storeSearchResult on Store {  banner  vanityName  divisionNumber  storeNumber  phoneNumber  showWeeklyAd  showShopThisStoreAndPreferredStoreButtons  distance  latitude  longitude  address {    addressLine1    addressLine2    city    countryCode    stateCode    zip  }  pharmacy {    phoneNumber  }}",
-			'variables': { 'searchText': "", 'filters': [] },
-			'operationName': "storeSearch"
-		},
-		resolveWithFullResponse: true
-	}
-	// set zip code in request object
-	storeSearchReq['body']['variables']['searchText'] = zip;
-	
+	// make new request object
+	var storeSearchReq = new ZipcodeRequest(zip);
 	// make the request
 	// (call brendan eich i dont give a fuck)
 	rp(storeSearchReq)
-		.then(function(response) {
-			console.log(response);		
-		})
-		.catch(function(error) {
-			console.log("Error getting stores by zip: " + error)
-		});
+	.then(function(response) {
+		var storesArr = response['data']['storeSearch']['stores'];
+		console.log(JSON.stringify(storesArr));
+	})
+	.catch(function(error) {
+		console.log(error);
+	});
 };
 
 var getAllStores = function() {
+	var ziparr = new Array();
 	for(var i = 0; i < ziplist.length; ++i) {
-		getStoresByZip(ziplist[i]);
+		setTimeout(getStoresByZip, currTimeout, ziplist[i]);
+		currTimeout += 30000;
 	}
 };
 
