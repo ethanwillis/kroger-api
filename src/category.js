24ae2f68cba58f5a80dd04d2b999e40e391b47ef
Ethan Willis <elwillis1@gmail.com>
2017-05-07 13:12:13 -0500

    WIP rework of category.js to use the new Query class

--- src/category.js
+++ src/category.js
@@ -4,4 +4,28 @@ class Category {
 		this.name = name;
 		this.id = id;
 	}
+
+	static getProducts(qc, category_id) {
+		var qc_options =  {
+			uri: "/storecatalog/clicklistbeta/api/products",
+			baseUrl: "https://www.kroger.com",
+			method: "GET",
+			jar: true,
+			qs: {
+				"searchTerm": "",
+				"categoryId": category_id,
+				"pageSize": "48",
+				"sort": "popularity"
+			}
+		}
+		qc.run_query(qc_options, function(err, result) {
+			if(err) {
+				console.log("Error running query: " + err);
+			} else {
+				console.log(JSON.stringify(result,null,"\t"))
+			}
+		})
+	}
 }
+
+module.exports = Category
